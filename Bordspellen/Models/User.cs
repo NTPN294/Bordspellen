ab51ffd41ef6f7c6224db5f41c607da01770efa8
NTPN <ntpn294@gmail.com>
2024-09-27 17:05:54 +0200

    loginscherm

---
+++ Bordspellen/Models/User.cs
@@ -0,0 +1,11 @@
+﻿
+public class User
+{
+    public int id { get; set; }
+    public String userName { get; set; }
+    public String email { get; set; }
+    public String password { get; set; }
+
+}
+
+
