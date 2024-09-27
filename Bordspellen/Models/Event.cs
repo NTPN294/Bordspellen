8da4acfa44d561a3412a67fcb9935c3493687e39
NTPN <ntpn294@gmail.com>
2024-09-27 16:33:06 +0200

    frontend worked on

---
+++ Bordspellen/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿
+public class Event
+{
+    public int Id { get; set; }
+    public String Host { get; set; }
+    public DateTime Date { get; set; }
+
+    public Event(int Id, String host, DateTime date)
+    {
+        this.Id = Id;
+        this.Host = host;
+        this.Date = date;
+    }
+
+}
--- Bordspellen/Pages/Index.cshtml.cs
+++ Bordspellen/Pages/Index.cshtml.cs
@@ -12,9 +12,21 @@ namespace Bordspellen.Pages
             _logger = logger;
         }
 
+
+        public List<Event> Events { get; set; }
         public void OnGet()
         {
+            Events = new List<Event>
+            {
+              new Event(1, "Alice", DateTime.Now.AddDays(5)),
+new Event(2, "Bob", DateTime.Now.AddDays(2)),
+new Event(3, "Charlie", DateTime.Now.AddDays(1)),
+new Event(4, "David", DateTime.Now.AddDays(-1)),
+new Event(5, "Eve", DateTime.Now.AddDays(-2)),
+new Event(6, "f", DateTime.Now.AddDays(-3)),
 
+
+            };
         }
     }
 }
